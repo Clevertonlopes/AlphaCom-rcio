6f6f9557f827b3a8637151cb3d8de90cc4ada2b9
Cleverton Lopes Souza <39173695+Clevertonlopes@users.noreply.github.com>
2019-11-14 23:18:25 -0300

    Add files via upload

---
+++ Correios Alfha/src/correios/alfha/Bairro.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package correios.alfha;
+import java.util.HashMap;
+/**
+ *
+ * @author Rodrigo
+ */
+public class Bairro {
+    // ATRIBUTOS
+    private String nomeBairro;
+    private HashMap<String,Logradouro> logradouros;
+    
+    // CONSTRUTOR
+    public Bairro(String nomeBairro) {
+        this.nomeBairro = nomeBairro;
+        this.logradouros = new HashMap();
+    }
+    
+    // GETTER E SETTERS
+    public String getNomeBairro() {
+        return nomeBairro;
+    }
+    public void setNomeBairro(String nomeBairro) {
+        this.nomeBairro = nomeBairro;
+    }
+    public HashMap<String, Logradouro> getLogradouros() {
+        return logradouros;
+    }
+    public void setLogradouros(HashMap<String, Logradouro> logradouros) {
+        this.logradouros = logradouros;
+    }
+
+    @Override
+    public String toString() {
+        return this.nomeBairro;
+    }
+    
+}
