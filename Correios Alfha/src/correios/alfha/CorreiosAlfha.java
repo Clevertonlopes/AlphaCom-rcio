32250b170103097a3aee4d8e7ee27554b2a2c951
brennodacostadantas <56850570+brennodacostadantas@users.noreply.github.com>
2019-11-21 15:40:14 -0300

    Add files via upload

--- Correios Alfha/src/correios/alfha/CorreiosAlfha.java
+++ Correios Alfha/src/correios/alfha/CorreiosAlfha.java
@@ -5,9 +5,8 @@
  */
 package correios.alfha;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
+import java.time.LocalTime;
 import java.util.HashMap;
 
 /**
@@ -16,57 +15,33 @@ import java.util.HashMap;
  */
 public class CorreiosAlfha {
 
-	public static void main(String[] args) {
-		try {
-			BufferedReader leitor = new BufferedReader(new FileReader("Arquivos//DNE_GU_SE_LOGRADOUROS.TXT"));
-			String linha = leitor.readLine(), conteudo[], nomeCidade, nomeBairro, cep, nomeLogradouro, siglaEstado;
-			HashMap<String, Logradouro> logradouros = new HashMap<String, Logradouro>();
-			HashMap<String, Estado> estados = new HashMap<String, Estado>();
-			HashMap<String, Cidade> cidades = new HashMap<String, Cidade>();
-			HashMap<String, Bairro> bairros = new HashMap<String, Bairro>();
-			Logradouro logradouro;
-			Cidade cidade;
-			Estado estado;
-			Bairro bairro;
-			int num_linha = 1;
-			while (linha != null) {
-				if (num_linha != 1) {
-					conteudo = linha.split(" \\s+");
-					siglaEstado = conteudo[0];
-					nomeCidade = conteudo[1];
-					nomeBairro = conteudo[2];
-					nomeLogradouro = conteudo[6];
-					if (conteudo.length == 8) {
-						cep = conteudo[7];
-					} else {
-						cep = conteudo[8];
-					}
-					estado = new Estado(siglaEstado, siglaEstado);
-					cidade = new Cidade(nomeCidade, estado);
-					bairro = new Bairro(nomeBairro);
-					logradouro = new Logradouro(cep, cidade, estado, bairro, nomeLogradouro);
-					if (!logradouros.containsKey(cep)) {
-						logradouros.put(cep, logradouro);
-					}
-					if (!estados.containsKey(siglaEstado)) {
-						estados.put(siglaEstado, estado);
-					}
-					if (!cidades.containsKey(nomeCidade)) {
-						cidades.put(nomeCidade, cidade);
-					}
-					if (!bairros.containsKey(nomeBairro)) {
-						bairros.put(nomeBairro, bairro);
-					}
-				}
-				linha = leitor.readLine();
-				num_linha++;
-			}
-			for (Logradouro l : logradouros.values()) {
-				System.out.println(l);
-			}
-			leitor.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String[] args) {
+        String diretorio = "C://Users//Brenno//Downloads//Fixo//DNE_GU_AL_LOGRADOUROS.TXT";
+        String cep = "57309620NS";
+        LeitorArquivo lerArquivo = new LeitorArquivo(diretorio);
+        HashMap<String, Logradouro> logradourosCadastrados = null;
+        LocalTime tempo1, tempo2;
+        try {
+            lerArquivo.carregarArquivo();
+            logradourosCadastrados = lerArquivo.getLogradouros();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        tempo1 = LocalTime.now();
+        Logradouro log = logradourosCadastrados.get(cep);
+        tempo2 = LocalTime.now();
+        if (log == null) {
+            System.out.println("Logradouro nao encontrado!");
+        } else {
+            System.out.println("Buscando pelo CEP: " + cep);
+            System.out.println("\n" + log);
+            int horas = Math.abs(tempo2.getHour() - tempo1.getHour());
+            int minutos = Math.abs(tempo2.getMinute() - tempo1.getMinute());
+            int segundos = Math.abs(tempo2.getSecond() - tempo1.getSecond());
+            int nanos = Math.abs(tempo2.getNano() - tempo1.getNano());
+            LocalTime tempoTotal = LocalTime.of(horas, minutos, segundos, nanos);
+            System.out.println("Tempo gasto para encontrar o CEP: " + cep + " " + tempoTotal);
+        }
+
+    }
 }
--- Correios Alfha/src/correios/alfha/LeitorArquivo.java
+++ Correios Alfha/src/correios/alfha/LeitorArquivo.java
@@ -1,5 +1,80 @@
 package correios.alfha;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.HashMap;
+
 public class LeitorArquivo {
+
+    private HashMap<String, Logradouro> logradouros = new HashMap<>();
+    private HashMap<String, Bairro> bairros = new HashMap<>();
+    private HashMap<String, Cidade> cidades = new HashMap<>();
+    private HashMap<String, Estado> estados = new HashMap<>();
+    private String diretorio;
+
+    public LeitorArquivo(String diretorio) {
+        this.diretorio = diretorio;
+    }
+
+    public void carregarArquivo() throws IOException {
+        BufferedReader leitor = new BufferedReader(new FileReader(diretorio));
+        String linha = leitor.readLine(), conteudo[], nomeCidade, nomeBairro, cep, nomeLogradouro, siglaEstado;
+        Logradouro logradouro;
+        Cidade cidade;
+        Estado estado;
+        Bairro bairro;
+        int num_linha = 1;
+        while (linha != null && !linha.endsWith("#")) {
+            if (num_linha != 1) {
+                conteudo = linha.split(" \\s+");
+                siglaEstado = conteudo[0];
+                nomeCidade = conteudo[1];
+                nomeBairro = conteudo[2];
+                nomeLogradouro = conteudo[6];
+                if (conteudo.length == 8) {
+                    cep = conteudo[7];
+                } else {
+                    cep = conteudo[8];
+                }
+                estado = new Estado(siglaEstado, siglaEstado);
+                cidade = new Cidade(nomeCidade, estado);
+                bairro = new Bairro(nomeBairro);
+                logradouro = new Logradouro(cep, cidade, estado, bairro, nomeLogradouro);
+
+                if (!this.cidades.containsKey(nomeCidade)) {
+                    this.cidades.put(nomeCidade, cidade);
+                }
+                if (!this.bairros.containsKey(nomeBairro)) {
+                    this.bairros.put(nomeBairro, bairro);
+                }
+                if (!this.logradouros.containsKey(cep)) {
+                    this.logradouros.put(cep, logradouro);
+                }
+                if (!this.estados.containsKey(siglaEstado)) {
+                    this.estados.put(siglaEstado, estado);
+                }
+            }
+            linha = leitor.readLine();
+            num_linha++;
+        }
+        leitor.close();
+    }
+
+    public HashMap<String, Logradouro> getLogradouros() {
+        return logradouros;
+    }
+
+    public HashMap<String, Bairro> getBairros() {
+        return bairros;
+    }
+
+    public HashMap<String, Cidade> getCidades() {
+        return cidades;
+    }
+
+    public HashMap<String, Estado> getEstados() {
+        return estados;
+    }
 
 }
